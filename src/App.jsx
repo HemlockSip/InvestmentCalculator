91c7a1313141ece6e8d8768381afd9ba019cf8f9
HemlockSip <davide.tansini87@gmail.com>
2025-01-05 20:07:01 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from './component/Header.jsx';
+import UserInput from './component/UserInput.jsx';
+
+function App() {
+  return (
+    <>
+    <Header/>
+    <UserInput/>
+    </>
+  );
+}
+
+export default App
